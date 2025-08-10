900b9baadcf937efb1fa0e79324e4a5beb617499
johndriftlife <nathanjohnsxm@gmail.com>
2025-08-09 22:49:57 -0400

    Delete backend directory

--- backend/src/utils/time.js
+++
@@ -1,5 +0,0 @@
-export function isOverlap(aStart, aEnd, bStart, bEnd) {
-  const toM = (t)=>{ const [h,m]=t.split(':').map(Number); return h*60+m; }
-  const aS = toM(aStart), aE = toM(aEnd), bS = toM(bStart), bE = toM(bEnd);
-  return aS < bE && bS < aE;
-}
